2169ee0da026e0abd327627c66aa48bdba2c22c6
charlesvickery <charles.vickery@gmail.com>
2015-03-12 20:13:34 -0700

    randomized color generator

--- Website.py
+++ Website.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import random
+
+
 app = Flask(__name__)
+random.seed(0)
+words = ["red","blue","green","brown"]
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return words[random.randint(0,3)]
 
 if __name__ == "__main__":
     app.run()
-    
